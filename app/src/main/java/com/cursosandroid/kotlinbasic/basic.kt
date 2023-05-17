b76a96a28fae383355067bc88e6af062fa0f3bdd
Alej25 <85592431+Alej25@users.noreply.github.com>
2023-05-17 00:27:28 -0600

    notas del ecosistema

---
+++ app/src/main/java/com/cursosandroid/kotlinbasic/basic.kt
@@ -0,0 +1,18 @@
+package com.cursosandroid.kotlinbasic
+
+fun main (){
+   print("kotlin")
+    println()
+     var age = 50 // var es una variable que puede cambiar de valor
+        println(age)
+    age = 21
+     println(age)
+     val name = 80 // val es una variable que no puede cambiar de valor
+
+    // una forma de declarar variables:
+     var xd : String
+     xd= "esto_es_una_cadena_de_carcateres"
+        println(xd)
+
+
+}
